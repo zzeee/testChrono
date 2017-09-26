ef38baa7d7eada5873c105fe3c39991704b6696f
a.zienko <a.zienko@cognitive.ru>
2017-09-26 18:24:50 +0300

    работает отображение ответа из redux. Сага отправляет в php

--- nodeserver/indexrb.js
+++ nodeserver/indexrb.js
@@ -4,17 +4,20 @@
 
 const https = require('https')
 const http = require('http')
-const querystring = require('querystring')
-var amqp = require('amqplib/callback_api')
-var MongoClient = require('mongodb').MongoClient,
+//const querystring = require('querystring')
+const amqp = require('amqplib/callback_api')
+
+const rabbiturl = 'amqp://localhost'
+const mongourl = 'mongodb://localhost:27017/'
+const MongoClient = require('mongodb').MongoClient,
   assert = require('assert')
-var i = 0
-var dbconn = ''
-amqp.connect('amqp://localhost', function(err, conn) {
+
+let dbconn = ''
+amqp.connect(rabbiturl, (err, conn) => {
   if (!err && conn) {
-    console.log('Connected correctly to server AMQP')
+    //console.log('Connected correctly to server AMQP')
     conn.createChannel(
-      function(err, ch) {
+      (err, ch) => {
         var q = 'hello'
         ch.assertQueue(q, { durable: false })
         ch.consume(q, function(msg) {
@@ -71,12 +74,9 @@ const findDocuments = callback => {
     callback(docs)
   })
 }
-var url = 'mongodb://localhost:27017/'
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(mongourl, (err, db) => {
   assert.equal(null, err)
-  console.log('Connected correctly to server Mongo')
-  //    insertDocuments(db,console.log);
   dbconn = db
   //   findDocuments(db, console.log);
 })
@@ -89,60 +89,21 @@ const insertCsv = function(data, callback) {
   })
 }
 
-/*
-http  .createServer((req, res) => {
-    const param = 'file='
-    let stp = req.url.indexOf(param) + param.length
-    let etp = req.url.indexOf('&', stp)
-    let name = req.url.substring(stp, etp > 0 ? etp : req.url.length)
-    let qres = readList(name).then(
-      e => {
-        //  console.log("geted", e);
-
-        let res2wr = { nam: name, data: e }
-        //        console.log(res2wr, typeof res2wr);
-
-        //  insertCsv(res2wr,console.log);
-        findDocuments(ee => {
-          res.write(JSON.stringify(ee))
-          res.end()
-        })
-        //insertCsv([
-        //{a : 1}, {a : 2}, {a : 3}
-        //],console.log);
-        res.write(e)
-        //res.end();
-      },
-      er => {
-        res.write('No file')
-        res.end()
-      }
-    )
-  })
-  .listen(8081)
-*/
 function readList(url) {
   return new Promise((okres, rej) => {
-    //console.log(url,
-    if (url.indexOf('https') >= 0) srv = https
-    else srv = http
+    let srv = http
+    if (url.indexOf('https') >= 0) srv = https //  Урлы могут быть и http и https, выбираем нужный протокол
     let localAnswer = ''
     srv
       .get(url, res => {
         const { statusCode } = res
-        //console.log(statusCode)
-        if (statusCode != 200) rej(new Error('bad server answer')) // Сделать разбивку 404, итп
-
-        res.on('data', e => {
-          // console.log('.', e.length, e.toString())
-          localAnswer += e
+        if (statusCode !== 200) rej(new Error('bad server answer')) // Сделать разбивку 404, итп
+        res.on('data', e1 => {
+          localAnswer += e1
         })
+        res.on('end', () => okres(localAnswer))
         res.on('error', e => {
           rej(new Error('error'))
-        })
-        res.on('end', e => {
-          //console.log('ended')
-          okres(localAnswer)
         })
       })
       .on('error', e => {
