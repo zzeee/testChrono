7c64ce5d01114da77196892ce3a1f24a91f946bf
a.zienko <a.zienko@cognitive.ru>
2017-10-02 11:01:49 +0300

    мелкие правки

--- src/components/CsvTable.js
+++ src/components/CsvTable.js
@@ -1,46 +1,71 @@
 /**
  * Created by zzeee on 01.10.2017.
  */
-import React from 'react';
-import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table'
+import React from 'react'
+import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table'
 import PropTypes from 'prop-types'
 
-const FORMATCSVLINE=(props)=>{
-    let res = ''
-    if (props.line && props.line.length > 0) {
-        const qt = props.line
-        //    console.log('QTT', typeof qt, qt.length)
-        let i = 0;
-        res = qt.map(e => {
-            i++;
-            return <TableRowColumn key={i} style={{width: 120}}>{e}</TableRowColumn>
-        })
-    }
+/**
+ * Обработка одной строки
+ * @param props - строка с данными
+ * @returns {XML}
+ * @private /
+ */
 
-    return <TableRow>{res}</TableRow>
+const FORMATCSVLINE = props => {
+  let res = ''
+  if (props.line && props.line.length > 0) {
+    const qt = props.line
+    //    console.log('QTT', typeof qt, qt.length)
+    let i = 0
+    res = qt.map(e => {
+      i++
+      return (
+        <TableRowColumn key={i} style={{ width: 120 }}>
+          {e}
+        </TableRowColumn>
+      )
+    })
+  }
+
+  return <TableRow>{res}</TableRow>
 }
+/**
+ *
+ *  * @param maxwidth максимальная ширина таблицы
+ *  * @param data массив с данными
+ *  * @param url обрабатываемый url
+ * @returns {XML}
+ * @constructor
+ */
 
-const CsvTable=(props)=>{
-  let i=0;
-    const qres=(props.data && props.data.length>0?<Table bodyStyle={{overflow: 'visible'}}>
+const CsvTable = props => {
+  let i = 0
+  const qres =
+    props.data && props.data.length > 0 ? (
+      <Table bodyStyle={{ overflow: 'visible' }}>
         <TableBody>
-            {props.data.map(q => {
-                let sqlength = q.length; // Обход глюка в Table который не отоюражает стоблцы если их не было в первой строке. Делаем всю таблицу равной ширины
-                for (let y = 0; y < props.maxwidth - sqlength; y++) {
-                    q.push('');
-                }
+          {props.data.map(q => {
+            let sqlength = q.length // Обход глюка в Table который не отоюражает стоблцы если их не было в первой строке. Делаем всю таблицу равной ширины
+            for (let y = 0; y < props.maxwidth - sqlength; y++) {
+              q.push('')
+            }
 
-                i++;
-                return <FORMATCSVLINE key={i} line={q}/>
-            })}
-            </TableBody></Table>:<span>Нет информации для отображения</span>);
-    return (qres);
+            i++
+            return <FORMATCSVLINE key={i} line={q} />
+          })}
+        </TableBody>
+      </Table>
+    ) : (
+      <span>Нет информации для отображения</span>
+    )
+  return qres
 }
 
 CsvTable.propTypes = {
-    data: PropTypes.array,
-    url: PropTypes.string,
-    maxwidth: PropTypes.number
-};
+  data: PropTypes.array,
+  url: PropTypes.string,
+  maxwidth: PropTypes.number,
+}
 
 export default CsvTable
